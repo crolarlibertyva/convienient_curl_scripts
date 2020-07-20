7c246fb86f2914f87225d053439d65fe890e9e98
Larry Crochet <larry.crochet@libertyits.com>
2020-07-20 14:24:05 -0400

    more scripts

--- badauthredirect.sh
+++ badauthredirect.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "authorization at $AS_HOST"
-curl "$AS_HOST/authorization?client_id=$CLIENT_ID&response_type=code&scope=openid&redirect_uri=$REDIRECT_URI&state=state-296bc9a0-a2a2-4a57-be1a-d0e2fd9bb601"
+curl -v "$AS_HOST/authorization?client_id=$CLIENT_ID&response_type=code&scope=openid&redirect_uri=$BAD_REDIRECT_URI&state=state-296bc9a0-a2a2-4a57-be1a-d0e2fd9bb601"

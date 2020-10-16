6f16e44522000cf848721fc9f815ecd74273eee8
Larry Crochet <larry.crochet@libertyits.com>
2020-10-16 15:34:36 -0400

    update delete branch script

--- deletegrants.sh
+++ deletegrants.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Revoke at $AS_HOST"
+echo "email equals $OAUTH_USER_EMAIL"
 curl -v -X DELETE \
   -d "client_id=$CLIENT_ID" \
   -d "email=$OAUTH_USER_EMAIL" \
   -d "token=$ACCESS_TOKEN" \
+  -H "Content-type: application/x-www-form-urlencoded" \
   "$AS_HOST/grants"

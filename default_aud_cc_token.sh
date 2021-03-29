377c31adaaceaf1455b8b04e20585cf1d13eef54
Larry Crochet <larry.crochet@libertyits.com>
2021-03-29 16:03:15 -0400

    cc token tests

--- default_aud_cc_token.sh
+++ default_aud_cc_token.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-docker run --network oauth-proxy_default --rm vasdvp/lighthouse-auth-utils:latest \
+docker run --network oauth-proxy_default --rm lcauthutils:latest \
      auth-cc \
      --client-id=$CC_CLIENT_ID \
      --client-secret=$CC_CLIENT_SECRET \
-     --authorization-url=http://oauth-proxy:7100/oauth2/health/system/v1 \
+     --authorization-url=http://oauth-proxy:7100/oauth2/keycloak/v1 \
      --scope="launch/patient patient/Patient.read" \
      --launch=123v456 | jq

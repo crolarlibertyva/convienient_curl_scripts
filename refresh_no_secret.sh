d534c1d02e060abf24486ce7ff165eb361633644
Larry Crochet <larry.crochet@libertyits.com>
2022-02-16 21:53:44 -0500

    update refresh

--- refresh_no_secret.sh
+++ refresh_no_secret.sh
@@ -2,5 +2,5 @@ curl -v --location --request POST "$AS_HOST/token" \
 --header "Content-Type: application/x-www-form-urlencoded" \
 --data-urlencode "grant_type=refresh_token" \
 --data-urlencode "client_id=$CLIENT_ID" \
---data-urlencode "client_secret=$CLIENT_SECRET" \
 --data-urlencode "refresh_token=$1"
+
